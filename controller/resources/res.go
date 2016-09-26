9975e3a615a6be134dc5434dca8eb217af1596d3
Igor Ławicki <igor.lawicki@gmail.com>
2016-09-27 01:43:10 +0200

    res.go now can serve js files

--- controller/resources/res.go
+++ controller/resources/res.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"upendo/router"
 	"upendo/settings"
 )
@@ -16,65 +17,60 @@ type Resources map[string]interface{}
 func init() {
 	router.Add("GET", "/res/:file", Resources{}, "ImageResource")
 	router.Add("GET", "/css/:file", Resources{}, "CSSResource")
+	router.Add("GET", "/js/:file", Resources{}, "JSResource")
+}
+
+func (c Resources) SendResource() {
+	file := c["file"].(string)
+	if settings.IgnoreMapFiles && strings.HasSuffix(file, ".map") {
+		return
+	}
+
+	w := c["writer"].(*bytes.Buffer)
+	directory := c["directory"].(string)
+
+	f, err := os.Open(settings.StartDir + directory + "/" + file)
+	defer f.Close()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		router.RedirectToError(c, http.StatusNotFound)
+		return
+	}
+
+	var fbytes []byte
+	fbytes, err = ioutil.ReadAll(f)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		router.RedirectToError(c, http.StatusInternalServerError)
+		return
+	}
+
+	writer := bufio.NewWriter(w)
+	_, err = writer.Write(fbytes)
+	writer.Flush()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 }
 
 func (c Resources) ImageResource() {
 	// fix: set content type
-	w := c["writer"].(*bytes.Buffer)
-
-	f, err := os.Open(settings.StartDir + "res/" + c["file"].(string))
-	defer f.Close()
-
-	if err != nil {
-		router.RedirectToError(c, http.StatusNotFound)
-	}
-
-	var fbytes []byte
-	fbytes, err = ioutil.ReadAll(f)
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	writer := bufio.NewWriter(w)
-	_, err = writer.Write(fbytes)
-	writer.Flush()
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
+	c["directory"] = "res"
+	c.SendResource()
 }
 
 func (c Resources) CSSResource() {
-	w := c["writer"].(*bytes.Buffer)
-
 	c["Content-Type"] = "text/css"
+	c["directory"] = "css"
+	c.SendResource()
+}
 
-	f, err := os.Open(settings.StartDir + "css/" + c["file"].(string))
-	defer f.Close()
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	var fbytes []byte
-	fbytes, err = ioutil.ReadAll(f)
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	writer := bufio.NewWriter(w)
-	_, err = writer.Write(fbytes)
-	writer.Flush()
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+func (c Resources) JSResource() {
+	c["Content-Type"] = "application/javascript"
+	c["directory"] = "js"
+	c.SendResource()
 }
