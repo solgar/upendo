7ec686b16467531cf519097708593c1192ab32b9
Igor Ławicki <igor.lawicki@gmail.com>
2021-05-13 16:38:16 +0200

    Changed util functions for headers.

--- controller/util.go
+++ controller/util.go
@@ -1,8 +1,5 @@
 package controller
 
-import "github.com/solgar/upendo/router"
-
-func AddHeader(c interface{}, k, v string) {
-	cc := c.(router.Controller)
-	cc.AddHeader(k, v)
+func AddHeader(c map[string]interface{}, k, v string) {
+	c["headers"].(map[string]string)[k] = v
 }
